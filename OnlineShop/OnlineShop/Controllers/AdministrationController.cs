3c641eedcd7caffb189f8e72002865e906c38674
Nihad Hrustić <nihad.hrustic@edu.fit.ba>
2020-04-20 14:26:22 +0200

    jQuery included in new layout.

--- OnlineShop/OnlineShop/Controllers/AdministrationController.cs
+++ OnlineShop/OnlineShop/Controllers/AdministrationController.cs
@@ -131,12 +131,12 @@ namespace OnlineShop.Controllers
         }
         public IActionResult CancelOrder(int orderid)
         {
-            var order = _database.order.Find(orderid);
+            var order = _database.order.FirstOrDefault(a=>a.OrderID==orderid);
             order.OrderStatusID = 3;
             order.OrderStatus = _database.orderstatus.Find(3);
-            foreach(var x in _database.orderdetails.Where(a => a.OrderID == orderid).ToList())
+            foreach(var x in _database.orderdetails.Include(p => p.Product).Where(a => a.OrderID == orderid).ToList())
             {
-                x.Product.UnitsInStock += x.Quantity;
+                x.Product.UnitsInStock += x.Quantity;  //vracamo tu kolicinu u UnitsInStock jer je s tog mjesta prividno oduzeto, dok je narudzba potvrdjena. U slucaju da se narudzba odobri, onda se oduzima i iz poslovnica. U slucaju otkazivanja narudzbe, vraca se kolicina na UnitsInStock
             }
             _database.SaveChanges();
 
